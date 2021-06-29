be652a9370dbc8d422eecb32426d9bf38bffddd0
Mateus <trafalgarmateus@gmail.com>
2021-06-29 09:35:07 -0300

    adicionando os serializers para Posts e Coments

---
+++ redeSocial/socialmedia/serializers.py
@@ -0,0 +1,16 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Posts, Coments
+
+
+class ComentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coments
+        fields = ('body_text')
+
+
+class PostsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posts
+        fields = ('text', 'post_coments')
+
