3b79e2acad5dd98416af5f60c29250bbca71a611
Gustavo Albert <gustavoads.millionary@gmail.com>
2021-06-30 11:29:46 -0300

    alterando estrutura dos Models

--- redeSocial/socialmedia/models.py
+++ redeSocial/socialmedia/models.py
@@ -2,18 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class Coments(models.Model):
-    body_text = models.CharField(max_length=200)
+class Posts(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=500)
+
+    class Meta:
+        ordering = ('text',)
 
     def __str__(self):
         return self.text
 
-class Posts(models.Model):
-    text = models.CharField(max_length=500)
-    post_coments = models.ForeignKey(Coments, related_name='coments', on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ('text',)
+class Coments(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    body_text = models.CharField(max_length=200)
+    postId = models.ForeignKey(Posts, related_name='comments', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
