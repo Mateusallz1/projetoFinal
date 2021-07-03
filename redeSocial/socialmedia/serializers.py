fcb16b6222c296baf3da8d2268caeacdb2a5530c
Gustavo Albert <gustavoads.millionary@gmail.com>
2021-07-03 11:03:00 -0300

    alterando serializers

--- redeSocial/socialmedia/serializers.py
+++ redeSocial/socialmedia/serializers.py
@@ -6,11 +6,11 @@ from .models import Posts, Coments
 class ComentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coments
-        fields = ('body_text')
+        fields = ('comment_id','body_text','postId')
 
 
 class PostsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Posts
-        fields = ('text', 'post_coments')
+        fields = ('post_id','title','text')
 
