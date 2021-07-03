6b6bfd58f9c9e07e07a675bf60cbe8a349b821ca
Mateus <trafalgarmateus@gmail.com>
2021-07-03 10:14:29 -0300

    mapeando json de post e fazendo requisicao da lista de posts

--- project_api_consumer/lib/main.dart
+++ project_api_consumer/lib/main.dart
@@ -1,4 +1,21 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+Future<List<Post>> fetchPosts(http.Client client) async {
+  final response =
+    await client.get(Uri.parse('http://localhost:8000/posts/'));
+  return compute(parsePost, response.body);
+}
+
+
+List<Post> parsePost(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Post>((json) => Post.fromJson(json)).toList();
+}
+
 
 void main() {
   runApp(MyApp());
@@ -14,6 +31,23 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       
+    );
+  }
+}
+
+
+class Post {
+  final int postid;
+  final String title;
+  final String text;
+
+  Post({this.postid, this.text, this.title,});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      postid: json['postid'] as int,
+      title: json['title'] as String,
+      text: json['text'] as String,
     );
   }
 }
